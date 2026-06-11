012f0e1c7f7c25625ca5f222460e6fdc1c82ea72
JayTodd <joshuabrigh949gmail.com>
2026-06-10 23:17:28 -0700

    update Hello World program with Triangle and Table and Chairs

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,19 +4,19 @@ public class HelloWorld{
 
         public static void main(String[] args) {
 
-            System.out.println("      T");
-            System.out.println("     TTT");
-            System.out.println("    TTTTT");
-            System.out.println("   TTTTTTT");
-            System.out.println("  TTTTTTTTT");
-            System.out.println(" TTTTTTTTTTT");
-            System.out.println("TTTTTTTTTTTTT");
+                    System.out.println("      T");
+                    System.out.println("     TTT");
+                    System.out.println("    TTTTT");
+                    System.out.println("   TTTTTTT");
+                    System.out.println("  TTTTTTTTT");
+                    System.out.println(" TTTTTTTTTTT");
+                    System.out.println("TTTTTTTTTTTTT");
+            System.out.println("X                   X");
+            System.out.println("X                   X");
+            System.out.println("X                   X");
+            System.out.println("XXXXX   XXXXXXXXX   XXXXX");
+            System.out.println("X   X   X       X   X   X");
+            System.out.println("X   X   X       X   X   X");
 
-                    System.out.println("X                   X");
-                    System.out.println("X                   X");
-                    System.out.println("X                   X");
-                    System.out.println("XXXXX   XXXXXXXXX   XXXXX");
-                    System.out.println("X   X   X       X   X   X");
-                    System.out.println("X   X   X       X   X   X");
-                }
-            }
+        }
+        }
